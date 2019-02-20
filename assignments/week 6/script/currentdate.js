fcd9e51c9919c90abac2a976b993c42e34e26e07
Mzovak <fra18029@byui.edu>
2019-02-19 22:01:50 -0500

    update

--- assignments/week 6/script/currentdate.js
+++ assignments/week 6/script/currentdate.js
@@ -3,4 +3,4 @@ var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-document.getElementById("currentdate").innerHTML = weeday[d.getDay()] + "," + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+document.getElementById("currentdate").innerHTML = weekday[d.getDay()] + "," + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
