0cbe27b889649a99bf77fab0d835f6c0e876d8f1
Mzovak <fra18029@byui.edu>
2019-02-21 14:53:17 -0500

    update

---
+++ assignments/week 7/script/windchill.js
@@ -0,0 +1,9 @@
+// Input - get windchill
+let x = document.getElementById('windchill').innerHTML;
+
+//Processing - calculate windchill
+let result = 35.74 + 0.6215(t) - 35.75(s)^0.16 + 0.4275(s)^16;
+    
+
+//Outpull - show windchill
+document.getElementById('output') = result
