2e05d33dbf8505f6e74c6adf9284ca4d01f2423f
Mzovak <fra18029@byui.edu>
2019-02-28 13:17:21 -0500

    updates

--- assignments/week 8/script/windchill.js
+++ assignments/week 8/script/windchill.js
@@ -8,5 +8,5 @@ var windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 *
    
 
 //Outpull - show windchill
-windChill = Math.round(windChill);
+windchill = Math.round(windchill);
 document.getElementById("windchill").innerHTML = windchill;
