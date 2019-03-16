fe3165052e118dcd1ff129007ce9261371561b08
Mzovak <fra18029@byui.edu>
2019-03-16 17:38:50 -0400

    push

--- assignments/week 10/script/weatherapi.js
+++ assignments/week 10/script/weatherapi.js
@@ -1,11 +1,11 @@
 let weatherRequest = new XMLHttpRequest();
 let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=22d3073ac0bc46c0166da31b57c37655&units=imperial';
-weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.send();
 
-weatherRequest.onload = function(){
+weatherRequest.onload = function () {
     let watherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
 }
