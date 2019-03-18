bc6304b7b74bcfd58dfcfbafb974ed01698a4b84
Mzovak <fra18029@byui.edu>
2019-03-18 15:01:23 -0400

    new week

---
+++ weathersite/script/weatherapi.js
@@ -0,0 +1,11 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=22d3073ac0bc46c0166da31b57c37655&units=imperial';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+}
